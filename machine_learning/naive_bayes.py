c9587c7953e5e4c9c6ebfeada2106999fb70d284
DGichuru <dangichuru77@gmail.com>
2021-08-21 21:54:18 +0300

    day 16

---
+++ machine_learning/naive_bayes.py
@@ -0,0 +1,16 @@
+#we will use iris dataset
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+#load the dataset
+data = load_iris()
+
+model = GaussianNB()
+model.fit(data.data, data.target)
+
+#evalaute
+print(model.score(data.data, data.target))
+
+#predict
+print(model.predict([4.2, 3, 0.9, 2.1])) #0 = setosa,1 = versicolor, and 2 = virginica
